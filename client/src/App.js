30493212be7cba27b4b3cf892700ef413578fe2c
mohammed <ibrahimdamy@gmail.com>
2019-11-12 03:36:52 +0100

    feature(create footer)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar/Navbar';
+// import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer'
 
 
 class App extends Component {
   render () {
     return (
-       <Navbar />
+       <Footer />
     );
   }
 }
