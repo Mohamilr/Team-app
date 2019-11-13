577512168e9059ab538b5bf8f76efef898c56a47
mohammed <ibrahimdamy7@gmail.com>
2019-11-13 17:20:31 +0100

    feature(gif layout)

--- client/src/containers/Timeline/Timeline.js
+++ client/src/containers/Timeline/Timeline.js
@@ -87,6 +87,66 @@ const Timeline = () => {
                     </div>
                 </div>
                 </div>
+                {/* gif area */}
+                <div className='gif-container'>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  <div className='gifs'>
+                    <img src='https://res.cloudinary.com/project-s/image/upload/v1573638728/q9koruze2ewqbhr5blk2.gif' alt='' className='gif-image' />
+                    <div className='article-properties'>
+                        <div><span className='by'>BY</span> <span>Mohammed Ibrahim</span></div>
+                        <p>comment icon <span>1</span></p>
+                    </div>
+                  </div>
+                  
+                </div>
             </div>
         </div>
     );
