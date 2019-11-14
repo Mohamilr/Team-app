7e8a58c03d25365d455e4ed00e768335eab71169
mohammed <ibrahimdamy7@gmail.com>
2019-11-14 11:29:36 +0100

    feature(gif page): create gif page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 // import Signin from './containers/Signin/Signin';
 // import Signup from './containers/Signup/Signup';
-import Timeline from './Containers/Timeline/Timeline';
- 
+// import Timeline from './Containers/Timeline/Timeline';
+import GifUpload from './Containers/GifUpload/GifUpload'; 
 
 class App extends Component {
   render () {
     return (
-      <Timeline />
+      <GifUpload />
+      // <Timeline />
       //  <Signin />
       //  <Signup />
     );
---
+++ client/src/containers/GifUpload/GifUpload.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import UserNav from '../../components/UserNav/UserNav';
+import './GifUpload.css';
+import '../../assets/Actions/Upload';
+
+const GifUpload = () => {
+  // const input = document.querySelector('.input');
+  // // const icon = document.querySelector('.fa-plus-circle');
+
+  // await function action () {
+  //   document.querySelector('.input').click();
+  // }
+  return (
+      <div className='container'>
+    <UserNav />
+       <div className='gif-upload-container'>
+           <h1>Upload A Gif</h1>
+           <div className='upload-gif'>
+           <input type='file' className='input' style={{display: 'none'}} />
+           <i className="fas fa-plus-circle" ></i>
+           <input type='text' placeholder='Gif title' />
+           <div className='preview'>
+             <p>gif name</p>
+             <p>gif size</p>
+           </div>
+           <button className='btn-upload'>Upload</button>
+           </div>
+       </div>
+      </div>
+  );
+};
+
+export default GifUpload;
