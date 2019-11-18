1657594cb655b338da5c1547d005ddc0f2e263a7
mohammed <ibrahimdamy7@gmail.com>
2019-11-18 13:51:26 +0100

    feature(article page): create article page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import Signin from './containers/Signin/Signin';
 // import Signup from './containers/Signup/Signup';
 // import Timeline from './Containers/Timeline/Timeline';
-import GifUpload from './Containers/GifUpload/GifUpload'; 
+// import GifUpload from './Containers/GifUpload/GifUpload'; 
+import ArticlePage from './Containers/ArticleUpload/ArticleUpload';
 
 class App extends Component {
   render () {
     return (
-      <GifUpload />
+      <ArticlePage />
+      // <GifUpload />
       // <Timeline />
       //  <Signin />
       //  <Signup />
